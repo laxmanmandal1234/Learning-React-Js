cc3827457fdc07f2a51245e53382336b799fc41b
Laxman Mandal <84460666+laxmanmandal1234@users.noreply.github.com>
2023-07-17 19:16:03 +0530

    completed todo app with localStorage
    Used localStorage to get last updates, useEffect hook, added add task , delete task features

---
+++ src/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Header() {
+  return (
+      <nav>DAILY GOALS TRACKER</nav>
+  )
+}
+
+export default Header;
--- src/Home.jsx
+++ src/Home.jsx
@@ -1,41 +1,68 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react';
+import "./Home.css";
+import Task from './Task';
 
 function Home() {
-  let inputStyle = {
-    padding: 20,
-    border: "1px solid rgb(0,0,0,0.4)",
-    margin: 5
+
+  //get initial tasks from the local storage of the browser
+  const initialTasks = localStorage.getItem("tasks")
+    ? JSON.parse(localStorage.getItem("tasks"))
+    : [];
+  
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [tasks, setTasks] = useState(initialTasks);
+  
+  const submitHandler = (event) => {
+    event.preventDefault();
+    setTasks([...tasks, { title, description }]);
+    setTitle("");
+    setDescription("");
   }
 
-  let btnStyle = {
-    padding: 20,
-    border: "1px solid rgb(0,0,0,0.4)",
-    borderRadius: 5,
-    backgroundColor: "#f1f1f1",
-    margin: 5
+  const deleteTask = (index) => {
+    const filteredArr = tasks.filter((item, i) => {
+      return i !== index;
+    });
+    setTasks(filteredArr);
   }
 
-  const [value, setValue] = useState(0);
-  
-  let incrementHandler = () => {
-    setValue(value + 1);
-  }
-  
-  let decrementHandler = () => {
-    setValue(value - 1);
-  }
+  useEffect(() => {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }, [tasks]);
 
   return (
-    <div>
-      <input
-        style={inputStyle}
-        type="number"
-        placeholder="Enter any number"
-        value={value}
-        readOnly
-      />
-      <button style={btnStyle} onClick={incrementHandler}> + </button>
-      <button style={btnStyle} onClick={decrementHandler}> - </button>
+    <div className="container">
+      <h1>Daily Tasks</h1>
+      <form onSubmit={submitHandler}>
+        <input
+          type="text"
+          name="title"
+          placeholder='Title'
+          value={title}
+          onChange={ (e) => setTitle(e.target.value) }
+        />
+        <textarea
+          name="description"
+          id="" cols="30"
+          rows="5"
+          placeholder='Description'
+          value={description}
+          onChange={ (e) => setDescription(e.target.value) }
+        ></textarea>
+        <button type='submit'>POST</button>
+      </form>
+      {
+        tasks.map((item, index) => (
+          <Task
+            title={item.title}
+            description={item.description}
+            key={index}
+            deleteTask={deleteTask}
+            index={index}
+          />
+        ))
+      }
     </div>
   );
 }
